ffe3ae42735a98a94425bd26ade9d538bcf7eb97
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-05-27 22:58:45 +0200

    benchmarking bangladesh

--- bangladesh_case/benchmarkingBangladesh.py
+++ bangladesh_case/benchmarkingBangladesh.py
@@ -241,7 +241,7 @@ def contingency(bench_fn, model_fn, bench_thres, model_thres, mask_fn, urban_fn,
 
 # glofris downscaling output
 model_fn = "/scratch-shared/edwinsut/finalizing_downscaling/using_strahler_order_6_not_sure/global/maps/inun_100-year_of_channel_storage_catch_06.tif.map.masked_out.map" # r'c:\Users\hcwin\OneDrive\IVM\2017\paper_costs\benchmarks\GLOFRIS\inun_dynRout_RP_00100.tif'
-model_fn = "/scratch-shared/edwinsut/finalizing_downscaling/using_strahler_order_8/global/maps/inun_100-year_of_channel_storage_catch_08.tif.map.masked_out.map"
+model_fn = "/scratch-shared/edwinsut/finalizing_downscaling/using_strahler_order_7/global/maps/inun_100-year_of_channel_storage_catch_07.tif.map.masked_out.map"
 
 
 # reference
