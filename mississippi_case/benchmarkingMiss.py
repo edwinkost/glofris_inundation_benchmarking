e1e4b0a28439d511c6adc473400b23b69ee15218
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-05-28 01:08:41 +0200

    benchmarking for mississippi_case

--- mississippi_case/benchmarkingMiss.py
+++ mississippi_case/benchmarkingMiss.py
@@ -225,7 +225,7 @@ def contingency(bench_fn, model_fn, bench_thres, model_thres, mask_fn, title, ma
 # In[44]:
 
 # glofris downscaling output
-model_fn = "/scratch-shared/edwinsut/finalizing_downscaling/using_strahler_order_5/global/maps/inun_100-year_of_channel_storage_catch_05.tif.map.masked_out.map" # r'c:\Users\hcwin\OneDrive\IVM\2017\paper_costs\benchmarks\flint\flint_rp100.tif'
+model_fn = "/scratch-shared/edwinsut/finalizing_downscaling/using_strahler_order_6/global/maps/inun_100-year_of_channel_storage_catch_06.tif.map.masked_out.map" # r'c:\Users\hcwin\OneDrive\IVM\2017\paper_costs\benchmarks\flint\flint_rp100.tif'
 
 # reference dataset
 bench_fn = "input_data/mississippi_1in100.tif"  # r'c:\Users\hcwin\OneDrive\IVM\2017\paper_costs\benchmarks\flint\flint_1in100.tif'
