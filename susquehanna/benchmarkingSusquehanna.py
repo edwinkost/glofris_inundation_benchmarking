d9911eda5d6ee9727baceef9cff717d211051bf2
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-05-28 21:21:48 +0200

    benchmarking for susquehanna

--- susquehanna/benchmarkingSusquehanna.py
+++ susquehanna/benchmarkingSusquehanna.py
@@ -239,7 +239,7 @@ clone_map = "input_data/inun_susquehanna_100.tif"
 model_warp_fn = None # r'c:\Users\hcwin\OneDrive\projects\1209884_GFRA\benchmark\inun_dynRout_RP_00100_warp.tif'
 
 title = "Susquehanna"
-hr, far, csi, x, y, cont_arr, flood1, flood2 = contingency(bench_fn, model_fn, 0.5, 0., mask_fn, title, masking = True, clone_map = clone_map)
+hr, far, csi, x, y, cont_arr, flood1, flood2 = contingency(bench_fn, model_fn, 0.25, 0., mask_fn, title, masking = True, clone_map = clone_map)
 
 #~ hr, far, csi, x, y, cont_arr, flood1, flood2 = contingency(bench_fn, model_fn, 0.25, 0., mask_fn, title, masking=True)
 
