510881ff4911a4b3620551772fe4496a05e9c993
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2017-05-29 16:02:36 +0200

    finalizing

--- st_louis_case/benchmarking_st_louis.py
+++ st_louis_case/benchmarking_st_louis.py
@@ -283,7 +283,7 @@ def contingency(bench_fn, model_fn, bench_thres, model_thres, mask_fn, title, ma
 # glofris downscaling output
 model_fn = "/scratch-shared/edwinsut/finalizing_downscaling/using_strahler_order_6_not_sure/global/maps/inun_100-year_of_channel_storage_catch_06.tif.map.masked_out.map" # r'c:\Users\hcwin\OneDrive\IVM\2017\paper_costs\benchmarks\stlouis\stlouis_rp100.tif'
 model_fn = "/scratch-shared/edwinsut/finalizing_downscaling/using_strahler_order_6/global/maps/inun_100-year_of_channel_storage_catch_06.tif.map.masked_out.map"
-model_fn = "input_data/stlouis_rp100.tif"
+#~ model_fn = "input_data/stlouis_rp100.tif"
 
 # reference
 bench_fn = "input_data/inun_stlouis2.tif" # r'c:\Users\hcwin\OneDrive\IVM\2017\paper_costs\benchmarks\stlouis\inun_stlouis2.tif'
