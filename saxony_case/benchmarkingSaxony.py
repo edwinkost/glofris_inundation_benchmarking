beef883429d2863fdf80a1eff25f837fc90b9241
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2017-05-30 11:37:41 +0200

    finalizing

--- saxony_case/benchmarkingSaxony.py
+++ saxony_case/benchmarkingSaxony.py
@@ -197,7 +197,9 @@ bench_fn = "input_data/inun_local_elbe_1km_MASKED.tif" # r'c:\Users\hcwin\OneDri
 mask_fn  = "input_data/mask.tif"                       # r'c:\Users\hcwin\OneDrive\IVM\2017\paper_costs\urban_2010\landuse_1_base_2010.tif'
 
 model_warp_fn = None # r'c:\Users\hcwin\OneDrive\projects\1209884_GFRA\benchmark\inun_dynRout_RP_00100_warp.tif'
-hr, far, csi, x, y, cont_arr = contingency(bench_fn, model_fn, 0.5, 0., mask_fn, "Saxony", masking = True)
+
+#~ hr, far, csi, x, y, cont_arr = contingency(bench_fn, model_fn, 0.5, 0., mask_fn, "Saxony", masking = True)
+hr, far, csi, x, y, cont_arr = contingency(bench_fn, model_fn, 0.5, 0.1, mask_fn, "Saxony", masking = True)
 
 
 # In[10]:
